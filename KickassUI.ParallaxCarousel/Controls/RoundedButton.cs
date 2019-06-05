52f8bb14a2cd36660e1dcaf54347c2a603355bdc
Kai <K232@users.noreply.github.com>
2019-06-05 17:00:33 +0200

    Clear 2 warnings about hiding inherited members
    Fixed these 2 warnings
    
    > KickassUI.ParallaxCarousel/Controls/RoundedButton.cs(49,49): Warning CS0108: 'RoundedButton.PaddingProperty' hides inherited member 'Button.PaddingProperty'. Use the new keyword if hiding was intended. (CS0108) (KickassUI.ParallaxCarousel)
    > 
    > KickassUI.ParallaxCarousel/Controls/RoundedButton.cs(26,26): Warning CS0108: 'RoundedButton.Padding' hides inherited member 'Button.Padding'. Use the new keyword if hiding was intended. (CS0108) (KickassUI.ParallaxCarousel)

--- KickassUI.ParallaxCarousel/Controls/RoundedButton.cs
+++ KickassUI.ParallaxCarousel/Controls/RoundedButton.cs
@@ -5,9 +5,9 @@ namespace KickassUI.ParallaxCarousel.Controls
 { 
     public class RoundedButton : Button
     {
-        public static readonly BindableProperty PaddingProperty = BindableProperty.Create(nameof(Padding), typeof(Thickness), typeof(RoundedButton), new Thickness(0, 0, 0, 0));
+        public new static readonly BindableProperty PaddingProperty = BindableProperty.Create(nameof(Padding), typeof(Thickness), typeof(RoundedButton), new Thickness(0, 0, 0, 0));
 
-        public Thickness Padding
+        public new Thickness Padding
         {
             get { return (Thickness)GetValue(PaddingProperty); }
             set { SetValue(PaddingProperty, value); }
